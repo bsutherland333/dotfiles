8e972d9a912784c8aa1ff513b046cd147b82e01b
Brandon Sutherland <brandonsutherland2@gmail.com>
2023-10-10 17:32:50 -0600

    Added Zsh config files

--- intall.sh
+++ intall.sh
@@ -10,8 +10,15 @@ rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 
 ## zathura ##
+mkdir -p "$HOME/.config/zathura"
 ln -sf "$HOME/dotfiles/zathura/zathurarc" "$HOME/.config/zathura"
 
 ## i3 ##
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+## zsh ##
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+
