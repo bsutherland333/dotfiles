6bac641d5fde0366d2e6986bb4f0e02609a3b8c6
Brandon Sutherland <brandonsutherland2@gmail.com>
2023-12-05 11:15:10 -0700

    Set LSP messages to not shift columns in insert mode

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -59,3 +59,11 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- keep columns from constantly shifting by updating in insert mode
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- delay update diagnostics
+    update_in_insert = true,
+  }
+)
