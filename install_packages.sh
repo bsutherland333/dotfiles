625f08f5bb68793ba440c5c662ba27cdce253aa8
Brandon Sutherland <brandonsutherland2@gmail.com>
2023-12-06 14:05:05 -0700

    Simplified install-packages.sh and added packages

--- install_packages.sh
+++ install_packages.sh
@@ -1,79 +1,68 @@
 #!/bin/bash
 
-# Function to detect the Linux distribution
-detect_distribution() {
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
-        DISTRIBUTION=$ID
-    elif [ -f /etc/lsb-release ]; then
-        . /etc/lsb-release
-        DISTRIBUTION=$DISTRIB_ID
-    else
-        DISTRIBUTION="unknown"
-    fi
-}
-
-# Function to install packages on Ubuntu
-install_ubuntu_packages() {
-    sudo apt-get update
-    sudo apt-get install -y "${@}"
-}
-
-# Function to install packages on Arch
-install_arch_packages() {
-    sudo pacman -Syu --noconfirm
-    sudo pacman -S --noconfirm "${@}"
-}
-
 # Function to check it a command exists
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Main script
-detect_distribution
+## Main script
+
+# Detect distro
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRIBUTION=$ID
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    DISTRIBUTION=$DISTRIB_ID
+else
+    DISTRIBUTION="unknown"
+fi
 
 case $DISTRIBUTION in
     ubuntu)
         echo "Detected Ubuntu. Installing packages using apt-get."
-        # Add any ppa repos required
         sudo apt-get update
+
+        # Add ppa repos
         sudo apt-get install software-properties-common -y
         sudo add-apt-repository ppa:neovim-ppa/stable -y
 
-        packages=(
-            "man-db"
-            "neovim"
-            "git"
-            "zsh"
-            "fzf"
-            "ripgrep"
-            #"diff-so-fancy"
-            "clangd"
-            "clang"
-            #"pyright"
-            "less"
-            "zplug"
-        )
-        install_ubuntu_packages "${packages[@]}"
+        # Install apt packages
+        sudo apt-get install \
+            man-db \
+            neovim \
+            git \
+            zsh \
+            fzf \
+            ripgrep \
+            #diff-so-fancy \
+            clangd \
+            clang \
+            less \
+            zplug \
+            -y
+
+        # Install pip packages
+        sudo apt-get install python3-pip -y
+        pip3 install pyright
         ;;
     arch)
         echo "Detected Arch Linux. Installing packages using pacman."
-        packages=(
-            "man"
-            "man-pages"
-            "neovim"
-            "git"
-            "zsh"
-            "fzf"
-            "ripgrep"
-            "diff-so-fancy"
-            "clang"
-            "pyright"
-            "less"
-            #"zplug"  # On AUR, setup yay first
-        )
-        install_arch_packages "${packages[@]}"
+
+        # Install packages
+        sudo pacman -Syu --noconfirm \
+            man \
+            man-pages \
+            neovim \
+            git \
+            zsh \
+            fzf \
+            ripgrep \
+            diff-so-fancy \
+            clang \
+            pyright \
+            less
+            #zplug  # On AUR, setup yay first
         ;;
     *)
         echo "Unsupported distribution: $DISTRIBUTION. Cannot install packages."
